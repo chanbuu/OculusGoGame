c6388873db9c307c0d746f4808d875554d7c67fd
UNT48 <buchap@masterpiece-imac.local>
2018-06-04 07:48:20 +0900

    Initial Commit

---
+++ Assets/App/Scripts/CustomOGManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomOGManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _leftHandAnchor;
+    private Transform _rightHandAnchor;
+    private Transform _centerEyeAnchor;
+
+
+    private bool _isControllerConnected = false;
+    void Awake(){
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update(){
+        if(OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote) || OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote)){
+            _isControllerConnected = true;
+        }else{
+            _isControllerConnected = false;
+        }
+
+        if(this.isActiveAndEnabled){
+            var controller = OVRInput.GetActiveController();
+            if(controller == OVRInput.Controller.LTrackedRemote){
+                
+            }
+        }
+    }
+}
